4f40129f1de6b7eb32ff60db8eb5f1252ecb4f81
samurai <samurai@MacBook-Pro-de-samurai.local>
2026-09-07 10:43:02 +0200

    Add CSR demo

---
+++ csr-demo/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+
+function App() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((data) => {
+        setUsers(data);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) return <p>Chargement...</p>;
+
+  return (
+    <ul>
+      {users.map((user) => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </ul>
+  );
+}
+
+export default App;
